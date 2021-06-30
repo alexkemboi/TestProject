7baf96e0f1bf06743529906ba0d5a5e3cc9f6fdb
alexkemboi4127 <alexkemboi97@yahoo.com>
2021-06-30 09:44:44 +0300

    added multiply function

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ function divide(a,b){
     const d = (a/b).toFixed(2);
     console.log('Divide', d);
 }
+function multiply(a,b){
+   const product=a*b;
+   console.log('product',product);
+   return product;
+}
 add(3,4);
 subtract(5,3);
 divide(10,2);
+multiply(10,2);
  
