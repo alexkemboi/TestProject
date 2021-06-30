336497be032dd07dcf864d78d1ff5f65fc7b54d1
alexkemboi4127 <alexkemboi97@yahoo.com>
2021-06-30 09:28:33 +0300

    added subtract function

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ function add(a,b){
    console.log('Sum',sum);
    return sum;
 }
-
+function subtract(a,b){
+   const sub=a-b;
+   console.log('sub',sub);
+   return sub;
+}
 add(3,4);
+subtract(5,3);
+ 
