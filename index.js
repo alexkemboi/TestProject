0c518b0318fc8a530459e82e6c0d8e4611102c85
ALEX KEMBOI <84083929+alexkemboi4127@users.noreply.github.com>
2021-06-30 09:50:38 +0300

    added multiply function (#2)
    * added multiply function
    
    * removed console

--- index.js
+++ index.js
@@ -12,9 +12,15 @@ function subtract(a,b){
 }
 function divide(a,b){
     const d = (a/b).toFixed(2);
-    console.log('Divide', d);
+   //  console.log('Divide', d);
+}
+function multiply(a,b){
+   const product=a*b;
+   console.log('product',product);
+   return product;
 }
 add(3,4);
 subtract(5,3);
 divide(10,2);
+multiply(10,2);
  
