5687e546ea2e4527977055fc6716c8992d08d571
maikofelix47 <maikofelix89@gmail.com>
2021-06-30 09:37:39 +0300

    Added divide function

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ function subtract(a,b){
    console.log('sub',sub);
    return sub;
 }
+function divide(a,b){
+    const d = (a/b).toFixed(2);
+    console.log('Divide', d);
+}
 add(3,4);
 subtract(5,3);
+divide(10,2);
  
